8c976235d5ff2c7e15713c838abbac53553baa45
soithangsing <getdatasoi@outlook.com>
2022-10-18 22:22:12 +0530

    imported font and added various colours

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// import the font and store all colours as CSS variable
+// import the font and store all colours as CSS variable - done
+
+// import profile pictures and establish a css layout with breakpoints
+
+// establish dom element links 
