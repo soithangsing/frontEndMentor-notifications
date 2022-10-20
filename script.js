3c33cba6be2269a5fc3be6df8567fc4577e7e6dd
soithangsing <getdatasoi@outlook.com>
2022-10-20 09:40:19 +0530

    Imported profile pictures and established CSS layout

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // import the font and store all colours as CSS variable - done
 
-// import profile pictures and establish a css layout with breakpoints
+// import profile pictures and establish a css layout with breakpoints done import profile pictures and establish css layout, not yet done with breakpoints
 
 // establish dom element links 
